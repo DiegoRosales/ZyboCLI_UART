cde15fd6f58f97b32ac969bc1a283e0697e91a69
Diego <diegorosales.25@gmail.com>
2020-05-01 14:12:11 +0200

    Added new features
     - Left/right arrow support
     - Delete key
     - Backspace key

--- source/zybo_uart_driver.c
+++ source/zybo_uart_driver.c
@@ -4,9 +4,10 @@
 // Many of these are taken from the Demo source code of FreeRTOS
 //////////////////////////////////////////////////////////////////
 
-/* Standard includes. */
-#include "string.h"
-#include "stdio.h"
+// C includes
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /* FreeRTOS includes. */
 #include "FreeRTOS.h"
@@ -21,17 +22,22 @@
 
 #include "zybo_uart_driver.h"
 
+/* Static Functions */
+static void prvUARTCommandConsoleTask( void *pvParameters );
+
 /* Const messages output by the command console. */
 static const char * const pcWelcomeMessage = "FreeRTOS command server.\r\nType Help to view a list of registered commands.\r\n\r\n>";
 static const char * const pcEndOfOutputMessage = "\r\n[Press ENTER to execute the previous command again]\r\n>";
 static const char * const pcNewLine = "\r\n";
+#if( EXEC_LAST_CMD_ON_EMPTY_RETURN == 1 )
+	static const char * const pcEndOfOutputMessage = "\r\n[Press ENTER to execute the previous command again]\r\n>> ";
+#else
+	static const char * const pcEndOfOutputMessage = "\r\n>> ";
+#endif
 
 /* Used to guard access to the UART in case messages are sent to the UART from
 more than one task. */
 static SemaphoreHandle_t xTxMutex = NULL;
-
-/* The handle to the UART port, which is not used by all ports. */
-static xComPortHandle xPort = 0;
 
 // This task comes from the Zynq CLI demo from the FreeRTOS source code
 // -------------
@@ -41,15 +47,32 @@ static xComPortHandle xPort = 0;
 // This task should be called from the scheduler
 static void prvUARTCommandConsoleTask( void *pvParameters )
 {
-    signed char    cRxedChar;                             // Character received 
+    signed char    cRxedChar;                             // Character received
     static char    cInputString[ cmdMAX_INPUT_SIZE ];     // This array will hold the characters received in a string
+    static char    cEscParamBytes[ cmdMAX_INPUT_SIZE ];   // This array will hold the escape command parameter bytes
+    static char    cEscInterBytes[ cmdMAX_INPUT_SIZE ];   // This array will hold the escape command intermeiate bytes
+    static char    cEscInitialByte;                       // This will hold the escape command initial byte
+    static char    cEscFinalByte;                         // This will hold the escape command final byte
     static char    cLastInputString[ cmdMAX_INPUT_SIZE ]; // This will hold the last command in case the user presses ENTER without any input (i.e. repeats the last command)
-    uint8_t        ucInputIndex = 0;                      // Idex pointer for cInputString
+    uint8_t        ucInputIndex        = 0;               // Idex pointer for cInputString
+    uint8_t        ucParamBytesIndex   = 0;               // Idex pointer for cInputString
+    uint8_t        ucInterBytesIndex   = 0;               // Idex pointer for cInputString
+    uint8_t        ucCurrInputStrinLen = 0;               // Idex pointer for cInputString
+    uint8_t        ucEscCount          = 0;               // Escape Character Count
+    uint8_t        ucReceivingEsc      = 0;               // Escape Character Count
     char           *pcOutputString;                       // Pointer to the buffer that will be used to store the CLI application output
     BaseType_t     xReturned;                             // This will hold the exit state of the CLI command (pdFALSE == Command is done generating output strings)
     xComPortHandle xPort;                                 // UART Port
+    char           *clear_screen = (char *) malloc(27);
 
 	( void ) pvParameters; // We are not using the pvParameters right now. Using this to avoid warnings.
+
+  sprintf( clear_screen, "%c[2J\n\r",27 );
+
+	/* Initialize the Input String */
+	memset( cInputString, 0x00, cmdMAX_INPUT_SIZE );
+	memset( cEscParamBytes, 0x00, cmdMAX_INPUT_SIZE );
+	memset( cEscInterBytes, 0x00, cmdMAX_INPUT_SIZE );
 
 	/* Obtain the address of the output buffer.  Note there is no mutual
 	exclusion on this buffer as it is assumed only one command console interface
@@ -60,6 +83,7 @@ static void prvUARTCommandConsoleTask( void *pvParameters )
 	xPort = xSerialPortInitMinimal( configCLI_BAUD_RATE, cmdQUEUE_LENGTH );
 
 	/* Send the welcome message. */
+	vSerialPutString( xPort, ( signed char * ) clear_screen, ( unsigned short ) strlen( clear_screen ) );
 	vSerialPutString( xPort, ( signed char * ) pcWelcomeMessage, ( unsigned short ) strlen( pcWelcomeMessage ) );
 
 	for( ;; )
@@ -72,29 +96,43 @@ static void prvUARTCommandConsoleTask( void *pvParameters )
 		/* Ensure exclusive access to the UART Tx. */
 		if( xSemaphoreTake( xTxMutex, cmdMAX_MUTEX_WAIT ) == pdPASS )
 		{
-			/* Echo the character back. */
-			xSerialPutChar( xPort, cRxedChar, portMAX_DELAY );
+			ucCurrInputStrinLen = strlen(cInputString);
+
+			/* Echo the character back unless it's a backspace and there's nothing there. */
+			if ( ((cmdIS_BACKSPACE(cRxedChar)  && ucInputIndex > 0) || (cRxedChar >= 0x20)) && (ucReceivingEsc == 0) && (ucCurrInputStrinLen == ucInputIndex) )  {
+				xSerialPutChar( xPort, cRxedChar, portMAX_DELAY );
+			}
 
 			/* Was it the end of the line? */
 			if( cRxedChar == '\n' || cRxedChar == '\r' )
 			{
-				/* Just to space the output from the input. */
-				vSerialPutString( xPort, ( signed char * ) pcNewLine, ( unsigned short ) strlen( pcNewLine ) );
+
 
 				/* See if the command is empty, indicating that the last command
 				is to be executed again. */
 				if( ucInputIndex == 0 )
 				{
-					/* Copy the last command back into the input string. */
-					strcpy( cInputString, cLastInputString );
+					#if( EXEC_LAST_CMD_ON_EMPTY_RETURN == 1 )
+						/* Copy the last command back into the input string. */
+						strcpy( cInputString, cLastInputString );
+						// Printout the command
+						vSerialPutString( xPort, ( signed char * ) cInputString, ( unsigned short ) strlen( cInputString ) );
+					#else
+						goto printEndOfOutputMessage;
+					#endif
 				}
+
+        /* Just to space the output from the input. */
+				vSerialPutString( xPort, ( signed char * ) pcNewLine, ( unsigned short ) strlen( pcNewLine ) );
 
 				/* Pass the received command to the command interpreter.  The
 				command interpreter is called repeatedly until it returns
 				pdFALSE	(indicating there is no more output) as it might
 				generate more than one string. */
+				memset(pcOutputString, '\0', configCOMMAND_INT_MAX_OUTPUT_SIZE);
 				do
 				{
+
 					/* Get the next output string from the command interpreter. */
 					xReturned = FreeRTOS_CLIProcessCommand( cInputString, pcOutputString, configCOMMAND_INT_MAX_OUTPUT_SIZE );
 
@@ -111,22 +149,117 @@ static void prvUARTCommandConsoleTask( void *pvParameters )
 				ucInputIndex = 0;
 				memset( cInputString, 0x00, cmdMAX_INPUT_SIZE );
 
-				vSerialPutString( xPort, ( signed char * ) pcEndOfOutputMessage, ( unsigned short ) strlen( pcEndOfOutputMessage ) );
+				printEndOfOutputMessage: vSerialPutString( xPort, ( signed char * ) pcEndOfOutputMessage, ( unsigned short ) strlen( pcEndOfOutputMessage ) );
 			}
 			else
 			{
-				if( cRxedChar == '\r' )
-				{
-					/* Ignore the character. */
-				}
-				else if( ( cRxedChar == '\b' ) || ( cRxedChar == cmdASCII_DEL ) )
+				if( cmdIS_BACKSPACE(cRxedChar) )
 				{
 					/* Backspace was pressed.  Erase the last character in the
 					string - if any. */
 					if( ucInputIndex > 0 )
 					{
-						ucInputIndex--;
-						cInputString[ ucInputIndex ] = '\0';
+						if ( ucCurrInputStrinLen > ucInputIndex ) {
+							// Go back one index
+							ucInputIndex--;
+							xSerialPutChar( xPort, '\b', portMAX_DELAY );
+
+							// Shift left all chars from the new index to the end
+							for(uint8_t i = ucInputIndex; i < ucCurrInputStrinLen; i++) {
+								cInputString[ i ] = cInputString[i + 1];
+							}
+
+							// Save the current console pointer
+							vSerialPutString( xPort, ( signed char * ) cmdANSI_ESC_SAVE_CURSOR, strlen(cmdANSI_ESC_SAVE_CURSOR) );
+							// Print the new shifted string
+							vSerialPutString( xPort, ( signed char * ) &cInputString[ucInputIndex], ( ucCurrInputStrinLen - ucInputIndex ) );
+							xSerialPutChar( xPort, ' ', portMAX_DELAY );
+							// Restore the console pointer
+							vSerialPutString( xPort, ( signed char * ) cmdANSI_ESC_RESTORE_CURSOR, strlen(cmdANSI_ESC_RESTORE_CURSOR) );
+						} else {
+							ucInputIndex--;
+							cInputString[ ucInputIndex ] = '\0';
+							xSerialPutChar( xPort, ' ', portMAX_DELAY );
+							xSerialPutChar( xPort, '\b', portMAX_DELAY );
+						}
+					}
+				} else if ( cmdIS_DEL(cRxedChar) ) {
+					if ( ucCurrInputStrinLen > ucInputIndex ) {
+						for(uint8_t i = ucInputIndex; i < ucCurrInputStrinLen; i++) {
+							cInputString[ i ] = cInputString[ i + 1];
+						}
+						vSerialPutString( xPort, ( signed char * ) cmdANSI_ESC_SAVE_CURSOR, strlen(cmdANSI_ESC_SAVE_CURSOR) );
+						vSerialPutString( xPort, ( signed char * ) &cInputString[ucInputIndex], ( ucCurrInputStrinLen - ucInputIndex ) );
+						xSerialPutChar( xPort, ' ', portMAX_DELAY );
+						vSerialPutString( xPort, ( signed char * ) cmdANSI_ESC_RESTORE_CURSOR, strlen(cmdANSI_ESC_RESTORE_CURSOR) );
+					}
+				}
+				else if ( cmdIS_ESCAPE(cRxedChar) )
+				{
+					// Initialize the escape sequences
+					ucReceivingEsc = 1;
+					memset( cEscParamBytes, 0x00, cmdMAX_INPUT_SIZE );
+					memset( cEscInterBytes, 0x00, cmdMAX_INPUT_SIZE );
+					cEscInitialByte   = 0x00;
+					cEscFinalByte     = 0x00;
+					ucParamBytesIndex = 0;
+ 					ucInterBytesIndex = 0;
+				}
+				else if (ucReceivingEsc == 1) {
+					// Get the first byte
+					if (cRxedChar >= 0x40 && cRxedChar <= 0x5f) {
+						cEscInitialByte = cRxedChar;
+						ucReceivingEsc = 2;
+					} else {
+						ucReceivingEsc = 0;
+					}
+				}
+				else if (ucReceivingEsc == 2)
+				{
+					if (cEscInitialByte == '[') { // Control Sequence Introducer
+						// Get the subsequent bytes
+						if( cRxedChar >=  0x30 && cRxedChar <= 0x3f) {        // Parameter bytes
+							cEscParamBytes[ ucParamBytesIndex ] = cRxedChar;
+							ucParamBytesIndex++;
+						} else if( cRxedChar >=  0x20 && cRxedChar <= 0x2f) { // Intermediate bytes
+							cEscInterBytes[ ucInterBytesIndex ] = cRxedChar;
+							ucInterBytesIndex++;
+						} else if( cRxedChar >=  0x40 && cRxedChar <= 0x7e) { // Final byte
+							cEscFinalByte  = cRxedChar;
+							ucReceivingEsc = 0;
+						}
+
+						if ( ucReceivingEsc == 0 ) {
+							if ( cEscFinalByte == 'A' ) {
+								xSerialPutChar( xPort, 'U', portMAX_DELAY );
+							} else if ( cEscFinalByte == 'B' ) {
+								xSerialPutChar( xPort, 'D', portMAX_DELAY );
+							} else if ( cEscFinalByte == 'C' ) {
+								if ( ucInputIndex < ucCurrInputStrinLen ) {
+									vSerialPutString( xPort, ( signed char * ) cmdANSI_ESC_RIGHT, ( unsigned short ) strlen( cmdANSI_ESC_RIGHT ) );
+									ucInputIndex++;
+								}
+							} else if ( cEscFinalByte == 'D' ) {
+								if ( ucInputIndex > 0 ) {
+									vSerialPutString( xPort, ( signed char * ) cmdANSI_ESC_LEFT, ( unsigned short ) strlen( cmdANSI_ESC_LEFT ) );
+									ucInputIndex--;
+								}
+							} else if ( cEscFinalByte == '~' ) { // VT Sequence
+								if (ucParamBytesIndex == 0) goto giveMutexSemaphore; // Must have a parameter
+
+								if ( cEscParamBytes[0] == '3' ) { // Delete
+									if ( ucCurrInputStrinLen > ucInputIndex ) {
+										for(uint8_t i = ucInputIndex; i < ucCurrInputStrinLen; i++) {
+											cInputString[ i ] = cInputString[ i + 1];
+										}
+										vSerialPutString( xPort, ( signed char * ) cmdANSI_ESC_SAVE_CURSOR, strlen(cmdANSI_ESC_SAVE_CURSOR) );
+										vSerialPutString( xPort, ( signed char * ) &cInputString[ucInputIndex], ( ucCurrInputStrinLen - ucInputIndex ) );
+										xSerialPutChar( xPort, ' ', portMAX_DELAY );
+										vSerialPutString( xPort, ( signed char * ) cmdANSI_ESC_RESTORE_CURSOR, strlen(cmdANSI_ESC_RESTORE_CURSOR) );
+									}
+								}
+							}
+						}
 					}
 				}
 				else
@@ -134,19 +267,40 @@ static void prvUARTCommandConsoleTask( void *pvParameters )
 					/* A character was entered.  Add it to the string entered so
 					far.  When a \n is entered the complete	string will be
 					passed to the command interpreter. */
-					if( ( cRxedChar >= ' ' ) && ( cRxedChar <= '~' ) )
-					{
-						if( ucInputIndex < cmdMAX_INPUT_SIZE )
+					#if( INPUT_IS_ASCII_ONLY == 1 )
+						if( ( cRxedChar >= ' ' ) && ( cRxedChar <= '~' ) )
 						{
-							cInputString[ ucInputIndex ] = cRxedChar;
+							if( ucInputIndex < cmdMAX_INPUT_SIZE )
+							{
+								cInputString[ ucInputIndex ] = cRxedChar;
+								ucInputIndex++;
+							}
+						}
+					#else
+						if( (ucInputIndex < cmdMAX_INPUT_SIZE) && (ucEscCount == 0) )
+						{
+
+							// If the cursor is in the middle of the string, move the right side of the string to accomodate the new char
+							if ( ucCurrInputStrinLen > ucInputIndex ) {
+								for (uint8_t i = ucCurrInputStrinLen; i > ucInputIndex; i--) {
+									cInputString[ i ] = cInputString[ i-1 ];
+								}
+								cInputString[ ucInputIndex ] = cRxedChar;
+								xSerialPutChar( xPort, cRxedChar, portMAX_DELAY );
+								vSerialPutString( xPort, ( signed char * ) cmdANSI_ESC_SAVE_CURSOR, strlen(cmdANSI_ESC_SAVE_CURSOR) );
+								vSerialPutString( xPort, ( signed char * ) &cInputString[ucInputIndex+1], ( ucCurrInputStrinLen - ucInputIndex + 1) );
+								vSerialPutString( xPort, ( signed char * ) cmdANSI_ESC_RESTORE_CURSOR, strlen(cmdANSI_ESC_RESTORE_CURSOR) );
+							} else {
+								cInputString[ ucInputIndex ] = cRxedChar;
+							}
 							ucInputIndex++;
 						}
-					}
+					#endif
 				}
 			}
 
 			/* Must ensure to give the mutex back. */
-			xSemaphoreGive( xTxMutex );
+			giveMutexSemaphore: xSemaphoreGive( xTxMutex );
 		}
 	}
 }
@@ -155,7 +309,7 @@ static void prvUARTCommandConsoleTask( void *pvParameters )
 // -------------
 // This function creates the prvUARTCommandConsoleTask
 // This is intended to be called in the initialization phases
-void vUARTCommandConsoleStart( uint16_t usStackSize, UBaseType_t uxPriority )
+void vUARTCommandConsoleStart( configSTACK_DEPTH_TYPE usStackSize, UBaseType_t uxPriority )
 {
 	/* Create the semaphore used to access the UART Tx. */
 	xTxMutex = xSemaphoreCreateMutex();
--- source/zybo_uart_driver.h
+++ source/zybo_uart_driver.h
@@ -2,14 +2,27 @@
 #define ZYBO_UART_DRIVER_h
 
 /* Dimensions the buffer into which input characters are placed. */
-#define cmdMAX_INPUT_SIZE		50
+#define cmdMAX_INPUT_SIZE		100
 
 /* Dimentions a buffer to be used by the UART driver, if the UART driver uses a
 buffer at all. */
 #define cmdQUEUE_LENGTH			25
 
 /* DEL acts as a backspace. */
-#define cmdASCII_DEL		( 0x7F )
+#define cmdASCII_BACKSPACE          ( 0x08 )
+#define cmdASCII_DEL                ( 0x7F )
+#define cmdASCII_ESC                ( 0x1B )
+#define cmdANSI_ESC_UP              ( (const char *) "\033[A" )
+#define cmdANSI_ESC_DOWN            ( (const char *) "\033[B" )
+#define cmdANSI_ESC_RIGHT           ( (const char *) "\033[C" )
+#define cmdANSI_ESC_LEFT            ( (const char *) "\033[D" )
+#define cmdANSI_ESC_SAVE_CURSOR     ( (const char *) "\033[s" )
+#define cmdANSI_ESC_RESTORE_CURSOR  ( (const char *) "\033[u" )
+
+/* Check if backspace is pressed */
+#define cmdIS_BACKSPACE(myChar) ( ( myChar == cmdASCII_BACKSPACE ) )
+#define cmdIS_DEL(myChar)       ( ( myChar == cmdASCII_DEL ) )
+#define cmdIS_ESCAPE(myChar)    ( ( myChar == cmdASCII_ESC ) )
 
 /* The maximum time to wait for the mutex that guards the UART to become
 available. */
@@ -19,9 +32,15 @@ available. */
 	#define configCLI_BAUD_RATE	115200
 #endif
 
-#define mainUART_COMMAND_CONSOLE_STACK_SIZE	( configMINIMAL_STACK_SIZE * 3UL )
+#define mainUART_COMMAND_CONSOLE_STACK_SIZE	( configMINIMAL_STACK_SIZE * 300UL )
 #define mainUART_COMMAND_CONSOLE_TASK_PRIORITY	( configMAX_PRIORITIES - 2 )
 
-static void prvUARTCommandConsoleTask( void *pvParameters );
+// If this flag is set to 1, the driver will ignore all non-character inputs
+#define INPUT_IS_ASCII_ONLY 0
+
+// If this flag is set to 1, if the user presses Return without typing anything, the last command will be executed
+#define EXEC_LAST_CMD_ON_EMPTY_RETURN 0
+
+void vUARTCommandConsoleStart( configSTACK_DEPTH_TYPE usStackSize, UBaseType_t uxPriority );
 
 #endif
